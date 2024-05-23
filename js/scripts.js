6b5e45dd8761ad868f699cd1097d2249b6606766
cod3ddy <edwardchirwa07@gmail.com>
2024-05-23 21:32:15 +0200

    added mailto: function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// javascript code
+
+const emailBtn = document.querySelector(".email-btn");
+//click event for the button
+
+emailBtn.addEventListener("click", () =>{
+    const emailLink = `mailto:edwardchirwa07@gmail.com`;
+
+    //redirect user to the mailto link 
+
+    window.location.href = emailLink;
+})
