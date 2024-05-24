61b3106f3693c49a0f6b7c9761bec67312d4e196
cod3ddy <edwardchirwa07@gmail.com>
2024-05-24 14:48:02 +0200

    added new ui

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,15 @@ emailBtn.addEventListener("click", () =>{
 
     window.location.href = emailLink;
 })
+
+//footer email btn
+const fEmailBtn = document.getElementById("footer-Email-Btn");
+
+
+fEmailBtn.addEventListener("click", () =>{
+    const emailLink = `mailto:edwardchirwa07@gmail.com`;
+
+    //redirect user to the mailto link 
+
+    window.location.href = emailLink;
+})
