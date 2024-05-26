535d9ab76dcb4a00a0f1710e8b9e914e612d2396
shadow <edwardchirwa07@gmail.com>
2024-05-25 18:23:32 -0800

    added qr code

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,21 @@
 const emailBtn = document.querySelector(".email-btn");
 //click event for the button
 
-emailBtn.addEventListener("click", () =>{
-    const emailLink = `mailto:edwardchirwa07@gmail.com`;
+emailBtn.addEventListener("click", () => {
+  const emailLink = `mailto:edwardchirwa07@gmail.com`;
 
-    //redirect user to the mailto link 
+  //redirect user to the mailto link
 
-    window.location.href = emailLink;
-})
+  window.location.href = emailLink;
+});
 
 //footer email btn
 const fEmailBtn = document.getElementById("footer-Email-Btn");
 
+fEmailBtn.addEventListener("click", () => {
+  const emailLink = `mailto:edwardchirwa07@gmail.com`;
 
-fEmailBtn.addEventListener("click", () =>{
-    const emailLink = `mailto:edwardchirwa07@gmail.com`;
+  //redirect user to the mailto link
 
-    //redirect user to the mailto link 
-
-    window.location.href = emailLink;
-})
+  window.location.href = emailLink;
+});
