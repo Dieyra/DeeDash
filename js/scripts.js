217dfe5143a330fb583366bac3e6f70edd37f56c
Cod3ddy[Shadow] <edwardchirwa07@gmail.com>
2024-05-31 18:54:51 +0200

    configured email button

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 //footer email btn
-const fEmailBtn = document.getElementById("emailBtnFooter");
+const emailBtn = document.querySelector(".container-fluid .emailBtn");
 
 // on button click
-fEmailBtn.addEventListener("click", () => {
+emailBtn.addEventListener("click", () => {
   const link = "mailto:edwarchirwa07@gmail.com";
 
   // open the gmail app
   window.location.href = link;
 });
-
-const hiBtn = document.getElementById("hiBtn");
-
 // when the "click  here" button has been clicked
