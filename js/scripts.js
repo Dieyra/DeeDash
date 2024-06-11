305137cb4a3cb2bc026665062d071013a32dfd90
Emily Billiati <emil2003.billiati@gmail.com>
2024-06-10 16:17:33 -0800

    changed some stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 //footer email btn
 const emailBtn = document.querySelector(".container-fluid .emailBtn");
 
-// on button click
+// on-button click
 emailBtn.addEventListener("click", () => {
-  const link = "mailto:edwarchirwa07@gmail.com";
+  const link = "mailto:edwarchirwa07@gmail.com;shadowyhands@gmail.com";
 
-  // open the gmail app
+  // open the gmail app (native app)
   window.location.href = link;
 });
-// when the "click  here" button has been clicked
